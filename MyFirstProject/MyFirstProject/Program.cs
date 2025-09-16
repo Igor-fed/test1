2d808cdc70468bb21743286c997e86c7a801eeee
Igor <gladovskij123@gmail.com>
2025-09-16 20:18:26 +0300

    Give a personalized hello.

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -10,7 +10,9 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World?");
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello ",name);
             Console.WriteLine();
         }
     }
